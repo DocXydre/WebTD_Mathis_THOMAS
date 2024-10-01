845dd8353ef5954344e22750de81b8fa8aa33347
DocXydre <totolego88@gmail.com>
2024-10-01 15:40:56 +0200

    oublie de commit

--- pw-td2/src/utils/flightCalculator.ts
+++ pw-td2/src/utils/flightCalculator.ts
@@ -1,8 +1,8 @@
-
+// Import le type Planet et  la classe Starship
 import { type Planet } from "../index";
 import { Starship } from "../Starship";
 
-export function calculateFlightDurationFromEarth(planet: Planet, starship: Starship, unity: string = "hours"): number {
+export function calculateFlightDurationFromEarth(planet: Planet, starship: Starship, unity?: string): number {
     let distance = planet.distanceFromEarth;
     let speed = starship.speed;
     let duration = distance / speed;
